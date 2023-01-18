c3bb314c0b6bf707ebea403c461d6438e78dfa63
Gevorg <vvmgev@gmail.com>
2023-01-18 15:41:52 +0400

    Added link to github

--- src/options/components/header/header.tsx
+++ src/options/components/header/header.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import GithubSVG from 'assets/icons/github.svg'
 import DocumentSVG from 'assets/icons/document.svg'
 
@@ -9,8 +8,8 @@ const Header =  () => {
         <div className="flex gap-5 p-5 py-7 h-30
             rounded-bl-3xl bg-slate-800 bg-opacity-40 drop-shadow-xl
             shadow-inner border-r border-l border-b border-slate-700">
-            <Link to="/" className="flex gap-3 items-center hover:text-sky-500"><span className="w-[24px]">{<GithubSVG />}</span>Github</Link>
-            <Link to="/" className="flex gap-3 items-center hover:text-sky-500"><span className="w-[24px]">{<DocumentSVG />}</span>Docs</Link>
+            <a target="_blank" href="https://github.com/vvmgev/Inssman" className="flex gap-3 items-center hover:text-sky-500"><span className="w-[24px]">{<GithubSVG />}</span>Github</a>
+            <a target="_blank" href="https://github.com/vvmgev/Inssman#documentation" className="flex gap-3 items-center hover:text-sky-500"><span className="w-[24px]">{<DocumentSVG />}</span>Docs</a>
         </div>
     </div>
 }
