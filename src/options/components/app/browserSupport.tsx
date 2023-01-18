90c9a5410b6b825904b21cf32e28aa44aed123cc
Gevorg <vvmgev@gmail.com>
2023-01-18 15:35:19 +0400

    Removed unused line

--- src/options/components/app/browserSupport.tsx
+++ src/options/components/app/browserSupport.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 const BrowserSupport = ({children}) => {
-  chrome.declarativeNetRequest.updateDynamicRules
-
   if(!chrome.declarativeNetRequest.updateDynamicRules || !chrome.declarativeNetRequest.getDynamicRules) {
     return <div className="py-5 px-5 text-lg rounded-3xl bg-slate-800 bg-opacity-40 drop-shadow-xl shadow-inner border-r border-t border-slate-700 w-full">
       <div>Please Update Your Browser Version</div>
