4ff3ae0f82adfdf6850b1a67ba024fb0fe77d730
Baghdasaryan <vvmgev@gamil.com>
2023-03-10 12:57:49 +0400

    Added condition for analytics

--- src/services/TrackService.ts
+++ src/services/TrackService.ts
@@ -4,15 +4,14 @@ import { PostMessageAction } from 'src/models/postMessageActionModel';
 
 class TrackService {
     constructor() {
+        if(process.env.NODE_ENV === 'development') return;
         chrome.runtime.sendMessage({ action: PostMessageAction.GetUserId }, ({ userid }) => {
             init('1e353dd7663e03056e8f96580e005504', String(userid));
         });
     };
 
     trackEvent(name: string) {
-        if(process.env.NODE_ENV === 'development') {
-            return;
-        }
+        if(process.env.NODE_ENV === 'development') return;
         try {
             track(name);
         } catch (error) {
