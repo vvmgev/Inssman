315594ceecf5ead23d90fcfc1d42687ba1f12ad9
Baghdasaryan <vvmgev@gamil.com>
2023-04-01 18:45:11 +0400

    Changed welocome page sizes

--- src/options/components/ruleList/ruleList.tsx
+++ src/options/components/ruleList/ruleList.tsx
@@ -25,7 +25,7 @@ export default () => {
       );
   };
 
-  return <div>
+  return <div className="h-[150px] min-h-[250px] mt-[50px]">
       <div className="rounded-tr-3xl rounded-bl-xl rounded-br-xl text-slate-200 rounded-tl-3xl bg-slate-800 bg-opacity-40 w-full border border-slate-700 min-h-[350px]">
         {!data.length && <div className="w-full h-full pl-5 py-5">
           <p className="text-2xl">Welcome to Inssman!</p>
