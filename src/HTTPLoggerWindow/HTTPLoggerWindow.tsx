0b83ea2adb896c22932d57a915c6b04be9f25688
Baghdasaryan <vvmgev@gamil.com>
2023-08-29 01:10:03 +0400

    Add strict mode

--- src/HTTPLoggerWindow/HTTPLoggerWindow.tsx
+++ src/HTTPLoggerWindow/HTTPLoggerWindow.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import HTTPLogger from 'src/options/pages/httpLogger/httpLogger';
 import Background from 'src/options/components/common/background/background';
@@ -8,12 +8,14 @@ import './HTTPLoggerWindow.css';
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-    <div className="h-screen w-screen overflow-hidden">
-        <Background>
-            <div className="m-[10px] h-full">
-                <HTTPLogger clientName={WebRequestClients.WINDOW}
-                            showOpenWindowBtn={false} />
-            </div>
-        </Background>
-    </div>
+    <StrictMode>
+        <div className="h-screen w-screen overflow-hidden">
+            <Background>
+                <div className="m-[10px] h-full">
+                    <HTTPLogger clientName={WebRequestClients.WINDOW}
+                                showOpenWindowBtn={false} />
+                </div>
+            </Background>
+        </div>
+    </StrictMode>
 );
