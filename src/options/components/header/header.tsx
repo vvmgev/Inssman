8001c0b1732e8351c6dec0afd39fbb355f305995
Baghdasaryan <vvmgev@gamil.com>
2023-08-29 12:41:41 +0400

    Add report bug button

--- src/options/components/header/header.tsx
+++ src/options/components/header/header.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GithubSVG from 'assets/icons/github.svg'
 import DocumentSVG from 'assets/icons/document.svg'
+import BugSVG from 'assets/icons/bug.svg'
 import ColorCover from '../common/colorCover/colorCover';
 
 
@@ -9,6 +10,7 @@ const Header =  () => {
         <ColorCover classes="flex gap-5 p-5 py-7 h-30 w-[initial] rounded-tl-none rounded-tr-none rounded-br-none">
             <a target="_blank" href="https://github.com/vvmgev/Inssman" className="flex gap-3 items-center hover:text-sky-500"><span className="w-[24px]">{<GithubSVG />}</span>Github</a>
             <a target="_blank" href="https://github.com/vvmgev/Inssman#documentation" className="flex gap-3 items-center hover:text-sky-500"><span className="w-[24px]">{<DocumentSVG />}</span>Docs</a>
+            <a target="_blank" href="https://github.com/vvmgev/Inssman/issues/new" className="flex gap-3 items-center hover:text-sky-500"><span className="w-[24px]">{<BugSVG />}</span>Report a Bug</a>
         </ColorCover>
     </div>
 }
