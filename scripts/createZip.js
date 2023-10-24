34b4a97e07ed1f19cf06e24ca7629dc97b4a5506
GevorgB <gevorg.baghdasaryan@sentium-consulting.com>
2023-10-24 13:24:50 +0400

    Update gitignore

--- scripts/createZip.js
+++ scripts/createZip.js
@@ -1,8 +1,12 @@
 const fs = require('fs');
+const path = require('path');
 const archiver = require("archiver");
 const { version } = require('../package.json');
+const outputDir = path.resolve(__dirname, "../", "release")
+const fileName = `inssman-${process.env.BROWSER}-${version}.zip`;
 
-const output = fs.createWriteStream(`./inssman-${process.env.BROWSER}-${version}.zip`);
+if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
+const output = fs.createWriteStream(path.join(outputDir, fileName));
 const archive = archiver("zip");
 output.on("close", () => archive.pointer());
 archive.on("error", (err) => {throw err});
