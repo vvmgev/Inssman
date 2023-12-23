3264f50c3a2243f9c41800a9cbebad1538f611be
Baghdasaryan <vvmgev@gamil.com>
2023-12-23 15:32:42 +0400

    Fix classes order

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -15,13 +15,13 @@ export default function Home() {
       </div>
       <div className="absolute top-0 right-0">
         <div className="bg-slate-800 bg-opacity-40 drop-shadow-xl rounded-3xl rounded-tl-none rounded-br-none rounded-tr-none shadow-inner border border-slate-700 overflow-auto flex gap-5 p-5 w-[initial]">
-          <a target="_blank" href="https://github.com/vvmgev/Inssman" className="flex gap-3 items-center hover:text-sky-500">
+          <a target="_blank" href="https://github.com/vvmgev/Inssman" className="flex items-center gap-3 hover:text-sky-500">
             <span className="w-[24px] text-white">
               {/* @ts-ignore */}
               <Image alt="Inssman Github" src={GithubSVG} />
             </span>
             Github</a>
-          <a target="_blank" href="https://github.com/vvmgev/Inssman#documentation" className="flex gap-3 items-center hover:text-sky-500">
+          <a target="_blank" href="https://github.com/vvmgev/Inssman#documentation" className="flex items-center gap-3 hover:text-sky-500">
               <span className="w-[24px] text-white">
                 <Image alt="Inssman Documentation" className="text-white" src={DocumentSVG} />
               </span>
@@ -33,14 +33,14 @@ export default function Home() {
       <div className="absolute top-[50%] left-[50%] text-center text-6xl -translate-x-2/4 -translate-y-2/4">
         <h1>COMING SOON 😍</h1>
         <hr />
-        <div className="mt-10 text-xl flex gap-2 flex-col">
-          <div className="flex flex-row gap-2 items-center justify-center">
+        <div className="flex flex-col mt-10 text-xl gap-2">
+          <div className="flex flex-row items-center justify-center gap-2">
             <a target="_blank" href="https://chromewebstore.google.com/detail/inssman-open-source-modif/ghlpdbkhlenlfiglgphledhfhchjfjfk" className="hover:text-sky-500">Download the extension for Google Chrome</a>
             <span className="w-5">
               <img alt="Chrome logo" src="https://github.com/vvmgev/Inssman/assets/11613729/338e9918-3a5a-45e6-96f5-6b0c1fdd94d7" />
             </span>
           </div>
-          <div className="flex flex-row gap-2 items-center justify-center">
+          <div className="flex flex-row items-center justify-center gap-2">
             <a target="_blank" href="https://microsoftedge.microsoft.com/addons/detail/inssman-opensource-mod/obibnmkbiilpbkhpkoagnkkjlghlndpf" className="hover:text-sky-500">Download the extension for Microsoft Egde</a>
             <span className="w-5">
               <img alt="Microsoft Edge logo" src="https://github.com/vvmgev/Inssman/assets/11613729/c15a2d97-a43c-4924-b7f1-6ba7992b9ae3" />
