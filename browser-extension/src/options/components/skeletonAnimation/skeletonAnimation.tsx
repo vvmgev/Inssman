50030ad70d8ef69bc7940b82dcf8613a1786e250
Baghdasaryan <vvmgev@gamil.com>
2023-12-23 18:17:47 +0400

    Change animation time

--- browser-extension/src/options/components/skeletonAnimation/skeletonAnimation.tsx
+++ browser-extension/src/options/components/skeletonAnimation/skeletonAnimation.tsx
@@ -4,7 +4,7 @@ const SkeletonAnimation = ({ children }) => {
       className="overflow-hidden relative space-y-5
                 bg-gradient-to-r from-transparent to-transparent py-2
                 shadow-xl shadow-black/5 before:absolute before:inset-0 before:-translate-x-full
-                before:animate-[shimmer_2.5s] before:border-t before:border-sky-400
+                before:animate-[shimmer_1.5s] before:border-t before:border-sky-400
                 before:bg-gradient-to-r before:from-transparent before:via-sky-400 before:to-transparent"
     >
       {children}
