2d49606ef1a15777dd66abd1380a68b93a642083
Baghdasaryan <vvmgev@gamil.com>
2024-01-03 17:31:26 +0400

    Remove comment code

--- browser-extension/src/options/options.tsx
+++ browser-extension/src/options/options.tsx
@@ -1,9 +1,7 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/app/app";
 import "./options.css";
 
 const container = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(container);
-// root.render(<StrictMode><App /></StrictMode>);
 root.render(<App />);
--- browser-extension/src/popup/popup.tsx
+++ browser-extension/src/popup/popup.tsx
@@ -1,9 +1,7 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/app";
 import "./popup.css";
 
 const container = document.getElementById("root") as HTMLDivElement;
 const root = createRoot(container);
-// root.render(<StrictMode><App /></StrictMode>);
 root.render(<App />);
