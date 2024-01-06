957f1e1d33e78ddf5102c53f255338c81921f7ca
Baghdasaryan <vvmgev@gamil.com>
2024-01-06 21:57:50 +0400

    Update dynamic imported components

--- web/src/pages/app/[...slug]/index.tsx
+++ web/src/pages/app/[...slug]/index.tsx
@@ -1,3 +1,3 @@
 import dynamic from "next/dynamic";
 const App = dynamic(() => import("../app"), { ssr: false });
-export default App;
+export default () => <App />;
--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,3 +1,4 @@
 import dynamic from "next/dynamic";
 const App = dynamic(() => import("./app"), { ssr: false });
-export default App;
+
+export default () => <App />;
