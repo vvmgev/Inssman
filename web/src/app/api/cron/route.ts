9d91a0a9b8a526584c92fb0d84aa08487ab20ab4
Baghdasaryan <vvmgev@gamil.com>
2024-01-13 17:00:11 +0400

    Change response of cron api

--- web/src/app/api/cron/route.ts
+++ web/src/app/api/cron/route.ts
@@ -1,16 +1,20 @@
 import { getRecordedSessions, removeRecordedSession, removeRecordedSessionFile } from "../../../config/firebase";
 
 export async function GET() {
-  const recordedSessions: any[] = await getRecordedSessions();
-
-  for (const recordedSession of recordedSessions) {
-    const ttlDate = new Date(recordedSession.ttl);
-    ttlDate.setDate(ttlDate.getDate() + 20);
-    if (new Date().getTime() > ttlDate.getTime()) {
-      await removeRecordedSession(recordedSession.docID);
-      await removeRecordedSessionFile(recordedSession.docID);
+  let isFaild = false;
+  try {
+    const recordedSessions: any[] = await getRecordedSessions();
+    for (const recordedSession of recordedSessions) {
+      const ttlDate = new Date(recordedSession.ttl);
+      ttlDate.setDate(ttlDate.getDate() + 20);
+      if (new Date().getTime() > ttlDate.getTime()) {
+        await removeRecordedSession(recordedSession.docID);
+        await removeRecordedSessionFile(recordedSession.docID);
+      }
     }
+  } catch (error) {
+    isFaild = true;
   }
 
-  return Response.json({ recordedSession: JSON.stringify(recordedSessions) });
+  return Response.json({ isFaild });
 }
