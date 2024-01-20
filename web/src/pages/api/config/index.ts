c566d22b584b25494852ef4b28529d040c185ac4
Baghdasaryan <vvmgev@gamil.com>
2024-01-20 13:31:05 +0400

    Enable toggle

--- web/src/pages/api/config/index.ts
+++ web/src/pages/api/config/index.ts
@@ -2,13 +2,10 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 type FeatureToggle = Record<string, boolean>;
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<FeatureToggle>
-) {
+export default function handler(req: NextApiRequest, res: NextApiResponse<FeatureToggle>) {
   res.status(200).json({
     featureOpenWebApp: false,
     featureUseCache: false,
-    featureShowRecord: false,
+    featureShowRecord: true,
   });
 }
