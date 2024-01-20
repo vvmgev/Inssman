830789a5fe596e8ed3c2932d02f977988c526599
Baghdasaryan <vvmgev@gamil.com>
2024-01-20 20:41:57 +0400

    Fix animation paddings

--- browser-extension/src/options/components/skeletonAnimation/skeletonAnimation.tsx
+++ browser-extension/src/options/components/skeletonAnimation/skeletonAnimation.tsx
@@ -1,8 +1,8 @@
 const SkeletonAnimation = ({ children }) => {
   return (
     <div
-      className="overflow-hidden relative space-y-5
-                bg-gradient-to-r from-transparent to-transparent py-2
+      className="relative space-y-5
+                bg-gradient-to-r from-transparent to-transparent
                 shadow-xl shadow-black/5 before:absolute before:inset-0 before:-translate-x-full
                 before:animate-[shimmer_1.5s] before:border-t before:border-sky-400
                 before:bg-gradient-to-r before:from-transparent before:via-sky-400 before:to-transparent"
