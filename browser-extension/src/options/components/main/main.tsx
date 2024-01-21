729846faf3cc59f66da6cb4735acc0e27dfc0684
Baghdasaryan <vvmgev@gamil.com>
2024-01-21 19:43:08 +0400

    Replace old modal with new and remove old package

--- browser-extension/src/options/components/main/main.tsx
+++ browser-extension/src/options/components/main/main.tsx
@@ -1,7 +1,6 @@
-import Popup from "reactjs-popup";
 import Input from "@options/components/common/input/input";
 import Button from "@options/components/common/button/button";
-import Section from "@options/components/common/section/section";
+import Dialog from "@options/components/dialog/dialog";
 import RuleList from "../ruleList/ruleList";
 import Icon from "@options/components/common/icon/icon";
 import { useState, useRef, useEffect } from "react";
@@ -10,22 +9,21 @@ import { IRuleMetaData } from "@models/formFieldModel";
 import { downloadFile } from "@utils/downloadFile";
 import { validateJSON } from "@utils/validateJSON";
 import { readFile } from "@utils/readFile";
-import "reactjs-popup/dist/index.css";
 
 export default () => {
   const importRulesRef = useRef<any>();
   const [search, setSearch] = useState<string>("");
   const [rules, setRules] = useState<IRuleMetaData[]>([]);
-  const [importFailed, setImportFailed] = useState<boolean>(false);
+  const [dialogName, setDialogName] = useState<string>("");
   const onHandleClearSearch = () => setSearch("");
   const onChangeSearch = (event) => setSearch(event.target.value);
   const onHandleImport = () => importRulesRef.current.click();
   const getRules = (): void => chrome.runtime.sendMessage({ action: PostMessageAction.GetStorageRules }, setRules);
   const onHandleExportRules = (): void =>
     chrome.runtime.sendMessage({ action: PostMessageAction.ExportRules }, (rules) => downloadFile(rules));
-  const onHandleDeleteRules = (close): void =>
+  const onHandleDeleteRules = (): void =>
     chrome.runtime.sendMessage({ action: PostMessageAction.DeleteRules }, () => {
-      close();
+      setDialogName("");
       getRules();
     });
 
@@ -38,7 +36,7 @@ export default () => {
           data,
         });
       } else {
-        setImportFailed(true);
+        setDialogName("importFailed");
       }
     });
   };
@@ -47,48 +45,64 @@ export default () => {
 
   return (
     <div className="min-h-[250px] overflow-hidden mx-[5%]">
-      <Popup
-        closeOnDocumentClick={true}
-        contentStyle={{ background: "transparent", border: "none" }}
-        open={importFailed}
-        onClose={() => setImportFailed(false)}
-        modal
-        position="right center"
-        overlayStyle={{ backdropFilter: "blur(1.5px)" }}
+      <Dialog
+        title="Import Failed"
+        visible={dialogName === "importFailed"}
+        onClose={() => setDialogName("")}
+        footer={
+          <div className="flex justify-end gap-3">
+            <Button
+              variant="outline"
+              trackName="Import faild - close"
+              className="min-w-[100px]"
+              onClick={() => setDialogName("")}
+            >
+              Close
+            </Button>
+          </div>
+        }
       >
-        {/* @ts-ignore */}
-        {(close: any) => (
-          <Section classes="bg-opacity-90 py-15">
-            <div className="flex pb-5 border-b border-slate-700">
-              <div className="flex-1 text-2xl text-slate-200">Import Failed</div>
-              <div className="flex justify-end flex-1">
-                <span onClick={close} className="w-[30px] cursor-pointer text-slate-200 hover:text-sky-500">
-                  <Icon name="cross" />
-                </span>
-              </div>
-            </div>
-            <div className="mt-10 text-2xl text-center text-slate-200">You have Invalid JSON file</div>
-            <div className="text-base text-center text-slate-500">
-              Please make sure you are uploading valid JSON file
-            </div>
-            <div className="mb-10 text-base text-center text-slate-500">
-              You can validate by this service &nbsp;
-              <a
-                className="underline cursor-pointer text-sky-500"
-                href="https://codebeautify.org/jsonvalidator"
-                target="_black"
-              >
-                Codebeautify.org
-              </a>
-            </div>
-            <div className="flex flex-row items-center justify-center gap-10 text-2xl text-slate-200">
-              <Button variant="outline" trackName="invalid JSON Close" className="min-w-[100px]" onClick={close}>
-                Close
-              </Button>
-            </div>
-          </Section>
-        )}
-      </Popup>
+        <div className="mt-10 text-2xl text-center text-slate-200">You have Invalid JSON file</div>
+        <div className="text-base text-center text-slate-500">Please make sure you are uploading valid JSON file</div>
+        <div className="mb-10 text-base text-center text-slate-500">
+          You can validate by this service &nbsp;
+          <a
+            className="underline cursor-pointer text-sky-500"
+            href="https://codebeautify.org/jsonvalidator"
+            target="_black"
+          >
+            Codebeautify.org
+          </a>
+        </div>
+      </Dialog>
+      <Dialog
+        title="Confirm Deletion"
+        visible={dialogName === "deleteAllRules"}
+        onClose={() => setDialogName("")}
+        footer={
+          <div className="flex justify-end gap-3">
+            <Button
+              variant="outline"
+              trackName="Delete All Rules - NO"
+              className="min-w-[100px]"
+              onClick={() => setDialogName("")}
+            >
+              No
+            </Button>
+            <Button
+              variant="outline"
+              startIcon={<Icon name="trash" />}
+              className="min-w-[100px] hover:text-red-400 hover:border-red-400"
+              trackName="Delete All Rules - YES"
+              onClick={() => onHandleDeleteRules()}
+            >
+              Yes
+            </Button>
+          </div>
+        }
+      >
+        <div className="my-10 text-2xl text-center text-slate-200">Are you sure want to delete all rules?</div>
+      </Dialog>
       <div className="w-full border rounded-tr-3xl rounded-bl-xl rounded-br-xl text-slate-200 rounded-tl-3xl bg-slate-800 bg-opacity-40 border-slate-700">
         <div className="py-5 max-h-[90%] w-full flex justify-between items-center px-6">
           <span className="flex flex-row items-center gap-2 text-lg">
@@ -123,59 +137,15 @@ export default () => {
                 Export
               </Button>
             </div>
-            <Popup
-              closeOnDocumentClick={true}
-              contentStyle={{ background: "transparent", border: "none" }}
-              trigger={
-                <Button
-                  variant="outline"
-                  className="hover:text-red-400 hover:border-red-400"
-                  trackName="Delete All Rules Popup"
-                  startIcon={<Icon name="trash" />}
-                >
-                  Delete All Rules
-                </Button>
-              }
-              modal
-              position="right center"
-              overlayStyle={{ backdropFilter: "blur(1.5px)" }}
+            <Button
+              onClick={() => setDialogName("deleteAllRules")}
+              variant="outline"
+              className="hover:text-red-400 hover:border-red-400"
+              trackName="Delete All Rules Popup"
+              startIcon={<Icon name="trash" />}
             >
-              {/* @ts-ignore */}
-              {(close: any) => (
-                <Section classes="bg-opacity-90 py-15">
-                  <div className="flex pb-5 border-b border-slate-700">
-                    <div className="flex-1 text-2xl text-slate-200">Confirm Deletion</div>
-                    <div className="flex justify-end flex-1">
-                      <span onClick={close} className="w-[30px] cursor-pointer text-slate-200 hover:text-sky-500">
-                        <Icon name="cross" />
-                      </span>
-                    </div>
-                  </div>
-                  <div className="my-10 text-2xl text-center text-slate-200">
-                    Are you sure want to delete all rules?
-                  </div>
-                  <div className="flex flex-row items-center justify-center gap-10 text-2xl text-slate-200">
-                    <Button
-                      variant="outline"
-                      trackName="Delete All Rules - NO"
-                      className="min-w-[100px]"
-                      onClick={close}
-                    >
-                      No
-                    </Button>
-                    <Button
-                      variant="outline"
-                      startIcon={<Icon name="trash" />}
-                      className="min-w-[100px] hover:text-red-400 hover:border-red-400"
-                      trackName="Delete All Rules - YES"
-                      onClick={() => onHandleDeleteRules(close)}
-                    >
-                      Yes
-                    </Button>
-                  </div>
-                </Section>
-              )}
-            </Popup>
+              Delete All Rules
+            </Button>
             <div className="text-sm">
               <Input
                 placeholder="Search By Rule Name"
